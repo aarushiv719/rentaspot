b11e18e7eaa1de7d83eaaba0876ad04170c8e2c2
KevinVCode <124304923+KevinVCode@users.noreply.github.com>
2023-04-25 21:42:54 -0400

    Add files via upload

---
+++ db_connection.js
@@ -0,0 +1,214 @@
+var mysql = require('mysql');  // MySQL database driver module
+var fs = require('fs');        // file system (fs) module
+
+// Create a connection to a MySQL database using
+// a specific set of credentials.
+function getDBconnection()
+{
+	// create the connection
+	// host - the name of the computer that is running the MySql database service.
+	// user - the name or user ID of the MySql user
+	// password - the pass word of the user account
+	// database - the database name
+	
+	var dbcon = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "@Kevin3rv",
+	  database: "RentASpot"
+	});
+	
+	return dbcon;
+}
+
+// Some of the SQL strings need to be enclosed in single quotes.
+function quote(value)
+{
+	return "'" + value + "'";
+}
+
+// Load a file into memory and send it to the res (HTML response)
+// function parameter so that it can be rendered in the remote 
+// HTML browswer.
+function loadFile(sFileName, bBinary, res, contentType)
+{
+	var bRtn = false;
+	
+	// Confirm the file exists.
+	if (fs.existsSync(sFileName))
+	{
+		// If the file is a binary file (like a jpeg image) then
+		// load it as a binary file.
+		if (bBinary === true)
+		{
+			fileToLoad = fs.readFileSync(sFileName);
+			res.writeHead(200, {'Content-Type':  contentType });
+			res.end(fileToLoad, 'binary');
+			bRtn = true;
+		}
+		else
+		{
+			fileToLoad = fs.readFileSync(sFileName, "utf8");
+			res.writeHead(200, {'Content-Type':  contentType });
+			res.write(fileToLoad);
+			res.end();
+			bRtn = true;
+		}
+	}
+	else
+	{
+		// If the file does not exist then let the end user know.
+		res.end('Invalid Request! File does not exist: ' + sFileName);
+	}	
+	
+	return bRtn;
+}
+
+// Given a set of user ID and password parameters, validate the UID and pw
+// and then using the res (HTML response) parameter, send the correct
+// HTML page to be rendereed on the remote browser.
+function validate_login_renter(uid, pw, res)
+{
+	var bRtn = false;
+	var dbcon = getDBconnection();
+	
+	// connect to the MySQL database
+	dbcon.connect();
+
+	// setup the SQL string
+	var sql = "select count(*) as recordcount from renter where Username = " + quote(uid) + " and Password = " + quote(pw) + ";";
+	
+	// perform the SQL query
+	dbcon.query(sql, function (err, rows, fields) 
+	{
+		if (err) 
+		{
+			console.log("sql is incorrect: " + sql);
+			throw err;
+		}
+
+		// If the SQL returned a single row using the UID and PW then
+		// the end-user typed in the correct PW and UID. If there are
+		// rows returned then the UID and PW is not valid.
+		bRtn = (rows[0].recordcount > 0 ? true : false);
+		console.log('found renter record: ' + rows[0].recordcount);
+		
+		if (bRtn == true)
+		{
+			// Validation is true therefore load the renter landing page.
+			loadFile('index_renter.html', false, res, 'text/html')
+		}
+		else
+		{
+			// Validattion is false, inform the end user
+			loadFile('login_invalid.html', false, res, 'text/html');
+		}			
+		
+		
+	});
+	
+	dbcon.end();
+	
+	return bRtn;
+}
+
+// Given a set of user ID and password parameters, validate the UID and pw
+// and then using the res (HTML response) parameter, send the correct
+// HTML page to be rendereed on the remote browser.
+function validate_login_propertyowner(uid, pw, res)
+{
+	var bRtn = false;
+	var dbcon = getDBconnection();
+	
+	// connect to the MySQL database
+	dbcon.connect();
+
+	// setup the SQL string
+	var sql = "select count(*) as recordcount from propertyowner where Username = " + quote(uid) + " and Password = " + quote(pw) + ";";
+	
+	// perform the SQL query
+	dbcon.query(sql, function (err, rows, fields) 
+	{
+		if (err) 
+		{
+			console.log("sql is incorrect: " + sql);
+			throw err;
+		}
+
+		// If the SQL returned a single row using the UID and PW then
+		// the end-user typed in the correct PW and UID. If there are
+		// rows returned then the UID and PW is not valid.
+		bRtn = (rows[0].recordcount > 0 ? true : false);
+		console.log('found property owner record: ' + rows[0].recordcount);
+		
+		if (bRtn == true)
+		{
+			// Validation is true therefore load the propertyowner landing page.
+			loadFile('index_propertyowner.html', false, res, 'text/html')
+		}
+		else
+		{
+			// Validattion is false, inform the end user
+			loadFile('login_invalid.html', false, res, 'text/html');
+		}			
+		
+		
+	});
+	
+	dbcon.end();
+	
+	return bRtn;
+}
+
+function parkingspotlisting(res)
+{
+	var dbcon = getDBconnection();
+	
+	// connect to the MySQL database
+	dbcon.connect();
+
+	// setup the SQL string
+	var sql = "select * from listings inner join propertyowner on listings.fkPropertyOwnerID = propertyowner.PropertyOwnerID order by ZipCode";
+		
+	dbcon.query(sql, function (err, rows, fields) 
+	{
+		if (err) 
+		{
+			console.log("sql is incorrect: " + sql);
+			throw err;
+		}
+
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write('<!DOCTYPE html>');
+		res.write('<html><body>');
+
+		res.write('<table><tr><th>Parking Spot Address</th><th>Parking Spot Size</th><th>Price</th><th>Map</th></tr>');
+		
+		for (var i = 0; i < rows.length; i++) 
+		{
+			res.write('<tr>');
+			res.write('<td>' + rows[i].Address + ' ' + rows[i].City + ' ' + rows[i].ZipCode + '</td>'); 
+			res.write('<td>' + rows[i].ParkingSpotLength + ' feet x ' + rows[i].ParkingSpotWidth + ' feet </td>');
+			res.write('<td>' + rows[i].Price + rows[i].PriceUOM + '</td>');
+			res.write('<td>Google map place holder</td>');
+			res.write('</tr>');
+		}
+		
+		res.write('</body></html>');
+	});
+	
+	dbcon.end();
+
+}
+
+
+// We can’t access the functions defined in one module in another module by
+// default. To access the module functions, we have to export the functions 
+// and import them in the file we want to call the functions.
+
+// Export the functions that are allowed to be called from other modules.
+module.exports = { 
+	validate_login_renter,
+	validate_login_propertyowner,
+	parkingspotlisting	
+};
---
+++ webserver.js
@@ -0,0 +1,228 @@
+/* -------------------------------------------------------------------------- *\
+   Node.js
+   
+   As an asynchronous event-driven JavaScript runtime, Node.js is designed to
+   build scalable network applications. In the following web server example, 
+   many client web connections can be handled concurrently. Upon each 
+   connection, the callback is fired, but if there is no work to be done, 
+   Node.js will sleep.
+
+   ----------------------------------------------------
+   Make sure you install the MySQL driver by navigating to the director that
+   contains the npm.exe file in the nodejs directory and then use the 
+   following command line:
+   
+   npm install mysql   
+   
+\* -------------------------------------------------------------------------- */
+// 1 - import Node.js core module
+// Load core Node.js modules
+const http = require('http');        // http module
+var fs = require('fs');              // file system (fs) module
+var db = require('./db_connection'); // mySQL connection file
+var qs = require('querystring');     // use this to process POST form variables
+
+
+// web server vars
+const ws_hostname = '127.0.0.1'; 
+const ws_port = 3000;
+
+
+// 2 - create server
+// The next step is to call createServer() method of http and specify callback 
+// function with request and response parameter.
+// The http.createServer() method includes request and response parameters 
+// which is supplied by Node.js.  The request object can be used to get 
+// information about the current HTTP request e.g., url, request header, and 
+// data. The response object can be used to send a response for a current HTTP 
+// request. 
+const server = http.createServer(function (req, res) 
+{
+	var pathname = req.url;
+	var isImage = 0, contentType, fileToLoad;
+	var file_ext, file_name;
+	
+	if (pathname.includes("."))
+	{
+		file_name = "." + pathname;
+		file_ext = pathname.split('.').pop();
+	}
+	else
+	{
+		file_name = "";
+		file_ext = "";
+	}
+	
+	if (file_ext != 'ico')
+	{
+		console.log('---------------------------------------------------');
+		console.log('method:    ' + req.method);
+		console.log('pathname:  ' + pathname);
+		console.log('file_ext:  ' + file_ext);
+		console.log('file_name: ' + file_name);
+	}
+	
+	if (file_ext.length > 0)
+	{
+		switch(file_ext)
+		{
+			case "jpg":
+				contentType = 'image/jpg';
+				isImage = 1;
+				break;
+			case "png":
+				contentType = 'image/png';
+				isImage = 1;
+				break;
+			case "js":
+				contentType = 'text/javascript';
+				isImage = 2;
+				break;
+			case "css":
+				contentType = 'text/css';
+				isImage = 2;
+				break;
+			case "html":
+				contentType = 'text/html';
+				isImage = 2;
+				break;
+		}
+	}	
+	
+	//check the URL of the current request
+	if (req.url == '/') 
+	{ 
+		// load the html file and send it to the client
+		fs.readFile('index.html',function (err, data)
+		{
+			if (!err)
+			{
+				res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+				res.write(data);
+				res.end();
+			}
+			else
+			{
+				res.writeHead(200, { 'Content-Type': 'text/html' });
+				res.write('<html><body><p>Failed to load requested file.</p></body></html>');
+				res.end();
+			}
+		});
+	}
+	else if (req.url == '/db_validate_login_renter' && req.method == 'POST') 
+	{ 
+		var body="";
+		req.on("data", function (data) { body +=data; });
+		req.on("end",function(){ 
+			var formvar = qs.parse(body);
+
+			var bValid = db.validate_login_renter(formvar.uid, formvar.psw, res);
+			console.log('db.validate_login_renter() = ' + bValid);
+
+		});
+	  
+		req.on("error",function(e){		});
+    }	
+	else if (req.url == '/db_validate_login_propertyowner' && req.method == 'POST') 
+	{ 
+		var body="";
+		req.on("data", function (data) { body +=data; });
+		req.on("end",function(){ 
+			var formvar = qs.parse(body);
+
+			var bValid = db.validate_login_propertyowner(formvar.uid, formvar.psw, res);
+			console.log('db.validate_login_propertyowner() = ' + bValid);
+
+		});
+	  
+		req.on("error",function(e){		});
+    }	
+	else if (req.url == '/parkingspotlisting' && req.method == 'GET') 
+	{ 
+		var body="";
+		req.on("data", function (data) { body +=data; });
+		req.on("end",function(){ 
+			var formvar = qs.parse(body);
+			db.parkingspotlisting(res);
+		});
+	  
+		req.on("error",function(e){		});
+    }	
+    else
+	{
+		if(isImage == 1)
+		{
+			loadFile(file_name, true, res, contentType);
+		}
+		else if(isImage == 2)
+		{
+			loadFile(file_name, false, res, contentType);
+		}
+		else
+			res.end('Invalid Request!');
+	}
+
+
+});
+
+// 3 - listen for any incoming requests
+// Finally, call listen() method of server object which was returned from 
+// createServer() method with port number, to start listening to incoming 
+// requests on port 5000.
+server.listen(ws_port, ws_hostname);
+console.log('Web server running at:     http://' + ws_hostname + ':' + ws_port + '/');
+console.log('Current working directory: ' + process.cwd());
+
+
+function getFormVars(req)
+{
+	var formvar;
+	
+	var body="";
+	req.on("data", function (data) {
+		body +=data;
+	});
+  
+	req.on("end",function(){
+		var formvar = qs.parse(body);
+		
+		//rest of code - this function is executed when all the variables are received
+	});
+  
+	req.on("error",function(e){
+		//console.log('problem with request: ' + e.message);
+	});
+	
+	return formvar;
+	
+}
+
+function loadFile(sFileName, bBinary, res, contentType)
+{
+	var bRtn = false;
+	
+	if (fs.existsSync(sFileName))
+	{
+		if (bBinary === true)
+		{
+			fileToLoad = fs.readFileSync(sFileName);
+			res.writeHead(200, {'Content-Type':  contentType });
+			res.end(fileToLoad, 'binary');
+			bRtn = true;
+		}
+		else
+		{
+			fileToLoad = fs.readFileSync(sFileName, "utf8");
+			res.writeHead(200, {'Content-Type':  contentType });
+			res.write(fileToLoad);
+			res.end();
+			bRtn = true;
+		}
+	}
+	else
+	{
+		res.end('Invalid Request! File does not exist: ' + sFileName);
+	}	
+	
+	return bRtn;
+}
